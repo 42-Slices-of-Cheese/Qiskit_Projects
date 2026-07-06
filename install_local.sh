031e7ed74e7f291dbc61327fd9c201e51c12af6f
Calvin Zeng <139883584+42-Slices-of-Cheese@users.noreply.github.com>
2026-07-06 01:07:41 +0000

    bulk testing update

---
+++ install_local.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source .venv/bin/activate
+pip install --upgrade pip
+pip install ipykernel qiskit qiskit-nature qiskit-ibm-runtime pyscf matplotlib pylatexenc matplotlib rdkit geometric

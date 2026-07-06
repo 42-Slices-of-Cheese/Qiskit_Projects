b942d8c42eb52d65a89f5b6a4698a72f3ddb2637
Calvin Zeng <139883584+42-Slices-of-Cheese@users.noreply.github.com>
2026-07-06 01:46:48 +0000

    anotter hotfix

--- install_local.sh
+++ install_local.sh
@@ -2,3 +2,4 @@
 source .venv/bin/activate
 pip install --upgrade pip
 pip install ipykernel qiskit qiskit-nature qiskit-ibm-runtime pyscf matplotlib pylatexenc matplotlib rdkit geometric git-lfs
+git lfs install
